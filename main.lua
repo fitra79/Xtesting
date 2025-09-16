f1094629f810f8f91769a2600149f016b50d1955
Mohd. Fitra Wahyudi <MFitraW@unitedtractors.com>
2025-09-16 15:17:33 +0700

    ♻️ : change url

--- main.lua
+++ main.lua
@@ -1,8 +1,7 @@
 -- ===[ CyberFrog Mini UI (LiteField) ]========================================
 -- Pastikan kamu sudah punya ModuleScript "LiteField" (dari canvas tadi)
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
-local LiteField = require(ReplicatedStorage:WaitForChild("LiteField")) -- ganti path jika beda
+local LiteField = loadstring(game:HttpGet("https://raw.githubusercontent.com/fitra79/Xtesting/refs/heads/main/main.lua"))() -- ganti path jika beda
 
 -- Buat Window utama
 local UI = LiteField.CreateWindow({
