132c25687defd53364770fa1adfa66345e87b9dc
Mohd. Fitra Wahyudi <MFitraW@unitedtractors.com>
2025-09-17 23:13:04 +0700

    ♻️ : fix bug

--- componen.lua
+++ componen.lua
@@ -502,8 +502,8 @@ function LiteField.CreateWindow(opts)
         
         -- Membuat TextBox untuk input
         local tb = Instance.new("TextBox")
-        btn.Size = UDim2.new(0, 84, 0, 28)
-        btn.Position = UDim2.new(1, -94, 0.5, -14)
+        tb.Size = UDim2.new(0, 84, 0, 28)
+        tb.Position = UDim2.new(1, -94, 0.5, -14)
         tb.PlaceholderText = opts2.Placeholder or ""
         tb.Text = tostring(opts2.Default or "")
         tb.TextColor3 = self._theme.Text
