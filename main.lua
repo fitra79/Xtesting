5004b45eb79c3bee2cc1bdb65d77d8a1347ada21
fitra79 <63347608+fitra79@users.noreply.github.com>
2025-09-21 23:57:40 +0700

    change again

--- main.lua
+++ main.lua
@@ -1,15 +1,18 @@
 -- ===[ CyberFrog Mini UI (LiteField) ]========================================
+-- Pastikan kamu sudah punya ModuleScript "LiteField" (dari canvas tadi)
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local LiteField = loadstring(game:HttpGet("https://raw.githubusercontent.com/fitra79/Xtesting/refs/heads/main/componen.lua"))()
+local LiteField = loadstring(game:HttpGet("https://raw.githubusercontent.com/fitra79/Xtesting/refs/heads/main/componen.lua"))() -- ganti path jika beda
 
+-- Buat Window utama
 local UI = LiteField.CreateWindow({
     Title = "CyberFrog Mini",
-    Keybind = Enum.KeyCode.K,
-    Theme = "Default",
-    ConfigName = "cyberfrog_mini"
+    Keybind = Enum.KeyCode.K,     -- tekan K untuk show/hide
+    Theme = "Default",            -- "Default" | "Light" | "Ocean" atau custom table
+    ConfigName = "cyberfrog_mini" -- opsional, untuk SaveConfig/LoadConfig
 })
 
+-- Notifikasi awal
 UI:Notify({
     Title = "Script Loaded!",
     Content = "Welcome to CyberFrog Mini Hub.",
@@ -17,32 +20,41 @@ UI:Notify({
 })
 
 -- ===[ Remote References ]====================================================
+local NetPackage = ReplicatedStorage:WaitForChild("Packages")
+    :WaitForChild("_Index")
+    :WaitForChild("sleitnick_net@0.2.0")
+    :WaitForChild("net")
+
+local EquipRodEvent         = NetPackage:WaitForChild("RE/EquipToolFromHotbar")
+local ChargeRodFunc         = NetPackage:WaitForChild("RF/ChargeFishingRod")
+local RequestMinigameFunc   = NetPackage:WaitForChild("RF/RequestFishingMinigameStarted")
+local FishingCompletedEvent = NetPackage:WaitForChild("RE/FishingCompleted")
+local SellItemFunc          = NetPackage:WaitForChild("RF/SellAllItems")
 
 
+local layout = Instance.new("UIListLayout", mainTab)
+layout.Padding = UDim.new(0, 8)  -- Menambahkan jarak 10px antar elemen
+layout.SortOrder = Enum.SortOrder.LayoutOrder
 
 -- ===[ Build Tabs & Elements ]===============================================
+-- pakai Icon asset Roblox, misalnya UI pack "rbxassetid://3926305904" (ikon menu)
+-- kamu bisa ganti dengan id/icon sesuai kebutuhan
 local mainTab = UI:AddTab({
-    Name = "Fishing",
-    Icon = "rbxassetid://10804731440"
+    Name = "Fishing",  -- Nama tab
+    Icon = "rbxassetid://10804731440"  -- ID ikon gambar
 })
 
-local teleportTab = UI:AddTab({
-    Name = "Teleport",
-    Icon = "rbxassetid://10804731440"
+local teloportTab = UI:AddTab({
+    Name = "Teleport",  -- Nama tab
+    Icon = "rbxassetid://10804731440"  -- ID ikon gambar
 })
 
-local mountTab = UI:AddTab({
-    Name = "Mount",
-    Icon = "rbxassetid://10804731440"
+local teloportTab = UI:AddTab({
+    Name = "Mount",  -- Nama tab
+    Icon = "rbxassetid://10804731440"  -- ID ikon gambar
 })
 
--- Tambah layout di mainTab
-local layout = Instance.new("UIListLayout")
-layout.Padding = UDim.new(0, 8)
-layout.SortOrder = Enum.SortOrder.LayoutOrder
-layout.Parent = mainTab.SectionFrame
 
--- === Fishing Section ===
 mainTab:AddSection("Auto Farming")
 
 _G.CyberFrog_AutoFishing = false
@@ -53,17 +65,12 @@ mainTab:AddToggle({
     Flag = "AutoFishToggle",
     Default = false,
     Callback = function(isOn)
-        local NetPackage = ReplicatedStorage:WaitForChild("Packages")
-            :WaitForChild("_Index")
-            :WaitForChild("sleitnick_net@0.2.0")
-            :WaitForChild("net")
-
-        local EquipRodEvent         = NetPackage:WaitForChild("RE/EquipToolFromHotbar")
-        local ChargeRodFunc         = NetPackage:WaitForChild("RF/ChargeFishingRod")
-        local RequestMinigameFunc   = NetPackage:WaitForChild("RF/RequestFishingMinigameStarted")
-        local FishingCompletedEvent = NetPackage:WaitForChild("RE/FishingCompleted")
-
         _G.CyberFrog_AutoFishing = isOn
+        UI:Notify({
+            Title = "Auto Fish",
+            Content = "Fitur Auto Fish " .. (isOn and "Diaktifkan" or "Dimatikan"),
+            Duration = 4
+        })
         if not isOn then return end
         task.spawn(function()
             while _G.CyberFrog_AutoFishing do
@@ -76,7 +83,6 @@ mainTab:AddToggle({
                     task.wait(2.1)
                     FishingCompletedEvent:FireServer()
                 end)
-                task.wait(0.5)
             end
         end)
     end
@@ -87,64 +93,103 @@ mainTab:AddToggle({
     Flag = "AutoSellToggle",
     Default = false,
     Callback = function(isOn)
-        local NetPackage = ReplicatedStorage:WaitForChild("Packages")
-            :WaitForChild("_Index")
-            :WaitForChild("sleitnick_net@0.2.0")
-            :WaitForChild("net")
-        local SellItemFunc          = NetPackage:WaitForChild("RF/SellAllItems")
         _G.CyberFrog_AutoSell = isOn
+        UI:Notify({
+            Title = "Auto Sell",
+            Content = "Fitur Auto Sell " .. (isOn and "Diaktifkan" or "Dimatikan"),
+            Duration = 4
+        })
         if not isOn then return end
         task.spawn(function()
             while _G.CyberFrog_AutoSell do
                 pcall(function()
                     SellItemFunc:InvokeServer()
                 end)
-                task.wait(3)
             end
         end)
     end
 })
 
--- === Teleport Section ===
-teleportTab:AddSection("Teleport")
+---- Teloport ----
+teloportTab:AddSection("Teleport")
 
 local player = Players.LocalPlayer
-local locations = {
-    Shop        = Vector3.new(42.11, 17.28, 2865.98),
-    Konoha      = Vector3.new(-600.73, 17.25, 512.04),
-    Lava        = Vector3.new(-616.90, 48.35, 186.99),
-    PulauHilang = Vector3.new(-3672.32, -135.07, -993.56)
-}
 
-for name, pos in pairs(locations) do
-    teleportTab:AddButton({
-        Name = name,
-        Callback = function()
-            local character = player.Character or player.CharacterAdded:Wait()
-            local root = character:WaitForChild("HumanoidRootPart", 5)
-            if root then
-                root.CFrame = CFrame.new(pos + Vector3.new(0, 3, 0))
-            else
-                warn("HumanoidRootPart tidak ditemukan. Gagal teleport.")
-            end
-        end
-    })
-end
+-- Target koordinat Shop
+local shop = Vector3.new(42.11, 17.28, 2865.98)
+local konoha = Vector3.new(-600.73, 17.25, 512.04)
+local lava = Vector3.new(-616.90, 48.35, 186.99)
+local pulauHilang = Vector3.new(-3672.32, -135.07, -993.56)
 
--- === Mount Section ===
-mountTab:AddSection("Mount")
 
-local function runLoader(urls)
-    for _, link in ipairs(urls) do
-        loadstring(game:HttpGet(link))()
-    end
-end
-
-mountTab:AddButton({
-    Name = "Mount Atin",
+teloportTab:AddButton({
+    Name = "Shop",
     Callback = function()
-        runLoader({
-            "https://raw.githubusercontent.com/fitra79/RbScript/refs/heads/main/maps/atin.lua"
+        UI:Notify({
+            Title = "Teleport Shop",
+            Content = "Fitur Teleport Shop Auto",
+            Duration = 4
         })
+        local character = player.Character or player.CharacterAdded:Wait()
+        local root = character:WaitForChild("HumanoidRootPart", 5)
+        if root then
+            root.CFrame = CFrame.new(shop + Vector3.new(0, 3, 0))
+        else
+            warn("HumanoidRootPart tidak ditemukan. Gagal teleport.")
+        end
+    end
+})
+
+teloportTab:AddButton({
+    Name = "Konoha",
+    Callback = function()
+        UI:Notify({
+            Title = "Teleport Konoha",
+            Content = "Fitur Teleport Konoha Auto",
+            Duration = 4
+        })
+        local character = player.Character or player.CharacterAdded:Wait()
+        local root = character:WaitForChild("HumanoidRootPart", 5)
+        if root then
+            root.CFrame = CFrame.new(konoha + Vector3.new(0, 3, 0))
+        else
+            warn("HumanoidRootPart tidak ditemukan. Gagal teleport.")
+        end
+    end
+})
+
+teloportTab:AddButton({
+    Name = "Lava",
+    Callback = function()
+        UI:Notify({
+            Title = "Teleport lava",
+            Content = "Fitur Teleport Lava Auto",
+            Duration = 4
+        })
+        local character = player.Character or player.CharacterAdded:Wait()
+        local root = character:WaitForChild("HumanoidRootPart", 5)
+        if root then
+            root.CFrame = CFrame.new(lava + Vector3.new(0, 3, 0))
+        else
+            warn("HumanoidRootPart tidak ditemukan. Gagal teleport.")
+        end
+    end
+})
+
+teloportTab:AddButton({
+    Name = "Pulau Hilang",
+    Callback = function()
+        UI:Notify({
+            Title = "Teleport Pulau Hilang",
+            Content = "Fitur Teleport Pulau Hilang Auto",
+            Duration = 4
+        })
+        local character = player.Character or player.CharacterAdded:Wait()
+        local root = character:WaitForChild("HumanoidRootPart", 5)
+        if root then
+            root.CFrame = CFrame.new(pulauHilang + Vector3.new(0, 3, 0))
+        else
+            warn("HumanoidRootPart tidak ditemukan. Gagal teleport.")
+        end
     end
 })
